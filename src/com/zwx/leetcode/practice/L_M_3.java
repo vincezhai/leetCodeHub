ec1642b882c25da08b6c8b3873271c724287cb40
vinceZ <435374846@qq.com>
2020-11-22 20:31:16 +0800

    test

--- src/com/zwx/leetcode/practice/L_M_3.java
+++ src/com/zwx/leetcode/practice/L_M_3.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 public class L_M_3 {
     public static void main(String[] args) {
+        // test for git
         String s = "abba";
         int res = lengthOfLongestSubstring(s);
         System.out.println("res = " + res);
