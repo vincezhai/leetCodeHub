c44758bba120d8d73d331b8504bd7b649402781d
vinceZ <435374846@qq.com>
2021-03-27 00:30:20 +0800

    bfs

---
+++ src/leetcode/editor/cn/ErChaShuDeShenDuLcof.java
@@ -0,0 +1,117 @@
+//输入一棵二叉树的根节点，求该树的深度。从根节点到叶节点依次经过的节点（含根、叶节点）形成树的一条路径，最长路径的长度为树的深度。 
+//
+// 例如： 
+//
+// 给定二叉树 [3,9,20,null,null,15,7]， 
+//
+//     3
+//   / \
+//  9  20
+//    /  \
+//   15   7 
+//
+// 返回它的最大深度 3 。 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 节点总数 <= 10000 
+// 
+//
+// 注意：本题与主站 104 题相同：https://leetcode-cn.com/problems/maximum-depth-of-binary-tre
+//e/ 
+// Related Topics 树 深度优先搜索 
+// 👍 101 👎 0
+
+package leetcode.editor.cn;
+
+import com.sun.imageio.plugins.wbmp.WBMPImageReader;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+* Java：二叉树的深度
+* @author zwx
+*/
+public class ErChaShuDeShenDuLcof{
+    public static void main(String[] args) {
+        Solution solution = new ErChaShuDeShenDuLcof().new Solution();
+        // TO TEST
+    }
+
+
+  class TreeNode {
+     int val;
+     TreeNode left;
+     TreeNode right;
+     TreeNode(int x) { val = x; }
+ }
+    
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+    class Solution {
+        /*public int maxDepth(TreeNode root) {
+            if(root == null){
+                return 0;
+            }
+            int[] res = new int[]{1};
+            return dfs(root, res);
+        }*/
+
+        /*private int dfs(TreeNode root, int[] res) {
+            int org = res[0];
+            if(root.left != null){
+                res[0]++;
+                dfs(root.left, res);
+            }
+            int tmp = res[0];
+            res[0] = org;
+            if(root.right != null){
+                res[0]++;
+                dfs(root.right, res);
+            }
+            return Math.max(res[0], tmp);
+        }*/
+
+        public int maxDepth(TreeNode root) {
+            if(root == null){
+                return 0;
+            }
+            int ans = 0;
+            Queue<TreeNode> queue = new LinkedList<>();
+            queue.offer(root);
+
+            while (!queue.isEmpty()){
+                int size = queue.size();
+                // 这个for循环的含义，每次只对同一层的数据进行处理（因为这里数层数，必须一层一层区分）
+                // 在一些岛屿题目中，更多的是侵占标记位置，没必要区分层次感，可以不用for循环分层，直接进入if判断
+                for (int i = 0; i < size; i++) {
+                    TreeNode node = queue.poll();
+                    if(node.left != null){
+                        queue.offer(node.left);
+                    }
+                    if(node.right != null){
+                        queue.offer(node.right);
+                    }
+                }
+                ans++;
+            }
+            return ans;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
