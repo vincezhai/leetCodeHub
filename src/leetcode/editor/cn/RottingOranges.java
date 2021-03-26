fc2d02273d2863532efb0a61b47b7b964f31e2dc
vinceZ <435374846@qq.com>
2021-03-27 00:29:49 +0800

    bfs

--- src/leetcode/editor/cn/RottingOranges.java
+++ src/leetcode/editor/cn/RottingOranges.java
@@ -47,6 +47,13 @@
 // 👍 340 👎 0
 
 package leetcode.editor.cn;
+
+import javafx.util.Pair;
+import org.omg.CORBA.INTERNAL;
+import sun.awt.image.ImageWatched;
+
+import java.util.*;
+
 /**
 * Java：腐烂的橘子
 * @author zwx
@@ -54,21 +61,89 @@ package leetcode.editor.cn;
 public class RottingOranges{
     public static void main(String[] args) {
         Solution solution = new RottingOranges().new Solution();
+        int[][] grid = {
+                {2,1,1},
+                {1,1,0},
+                {0,1,1}
+        };
+
+        int res = solution.orangesRotting(grid);
+        System.out.println("res = " + res);
+
         // TO TEST
     }
     
     //leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
-    public int orangesRotting(int[][] grid) {
+    class Solution {
+        private int row;
+
+        private int col;
+
+        private final int[] X_DIR = {0, 0, 1, -1};
+
+        private final int[] Y_DIR = {1, -1, 0, 0};
+
+        public int orangesRotting(int[][] grid) {
+            int res = 0;
+            // calc basic
+            this.row = grid.length;
+            this.col = grid[0].length;
+            if(grid == null || row == 0 || col == 0){
+                return 0;
+            }
+
+            // first detect bad point
+            Queue<Pair<Integer, Integer>> queue = new LinkedList<>();
+            HashMap<Pair<Integer, Integer>, Integer> map = new HashMap<>();
+            for (int i = 0; i < row; i++) {
+                for (int j = 0; j < col; j++) {
+                    if(grid[i][j] == 2){
+                        Pair<Integer, Integer> pair = new Pair<>(i, j);
+                        queue.offer(pair);
+                        map.put(pair, 0);
+                    }
+                }
+            }
+
+            // travel all bad point, update and record step by using hashMap
+            while (!queue.isEmpty()){
+                Pair<Integer, Integer> currPair = queue.poll();
+                int x = currPair.getKey();
+                int y = currPair.getValue();
+
+                for (int step = 0; step < 4; step++) {
+                    int xCopy = x + X_DIR[step];
+                    int yCopy = y + Y_DIR[step];
+                    Pair<Integer, Integer> tmpPair = new Pair<>(xCopy, yCopy);
+
+                    if(inGrid(xCopy, yCopy) && grid[xCopy][yCopy] == 1){
+                        queue.offer(tmpPair);
+                        // key statement
+                        res = map.get(currPair) + 1;
+                        map.put(tmpPair, res);
+                        grid[xCopy][yCopy] = 2;
+                    }
+                }
+
+            }
+
+            // judge result grid
+            for (int i = 0; i < row; i++) {
+                for (int j = 0; j < col ; j++) {
+                    if(grid[i][j] == 1){
+                        return -1;
+                    }
+                }
+            }
+            return res;
+        }
+
+        private boolean inGrid(int x, int y) {
+            return (x >= 0 && x < row && y >= 0 && y < col);
+        }
 
 
-        return 0;
     }
-
-
-
-
-}
 //leetcode submit region end(Prohibit modification and deletion)
 
 }
